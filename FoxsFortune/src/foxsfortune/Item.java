088ee2ca11f41012bff50e97ad4f111e6c6975c8
awsem <awsem@Tariq>
2026-05-31 12:44:44 -0400

    Finished the commentering on the Hitbox and item class

--- FoxsFortune/src/foxsfortune/Item.java
+++ FoxsFortune/src/foxsfortune/Item.java
@@ -14,24 +14,35 @@ package foxsfortune;
  * @author reesesanders
  */
 public class Item extends Entity {
-
+   // attrubutes
     private Boolean interactable;
+    // a Boolean that checks if the player can interact with the item
+ 
+
 
     public Item() {
+        // a Boolean that checks if the player can interact with the item
         super();
+        // calls the entity constructor because item is an entity
         this.interactable = Boolean.FALSE;
+        // starts as false because the item is not interactable by default
     }
 
     public Item(Boolean interactable) {
+        // constructor that lets interactable be set right away
         super();
+        // calls the entity constructor first
         this.interactable = interactable;
+        // stores the interactable value that was sent in
     }
 
     public void getInteractable() {
+        // prints if the item is interactable or not
         System.out.println("Interactable: " + interactable);
     }
 
     public Boolean setInteractable(Boolean b) {
+        // changes if the item is interactable and returns the new value
         this.interactable = b;
         return interactable;
     }
