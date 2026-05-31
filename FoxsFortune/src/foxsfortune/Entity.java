e53b0a79197b90ec50d66f9254be3a4c45fd5e6f
awsem <awsem@Tariq>
2026-05-31 10:49:53 -0400

    Finished the commentering on the Entity class

--- FoxsFortune/src/foxsfortune/Entity.java
+++ FoxsFortune/src/foxsfortune/Entity.java
@@ -12,65 +12,67 @@ package foxsfortune;
  */
 
 public class Entity {
-
-    private String name;
-    private int xPos;
-    private int yPos;
-    private boolean moving;
-    private Hitbox hitbox;
+    //attrubutes
+    private String name;  // a string that stores the name of the entity
+    private int xPos; // integer that stores the x position on the screen
+    private int yPos; // integer that stores the y position on the screen
+    private boolean moving; // a boolean that checks if the entity is moving or not
+    private Hitbox hitbox; // a hitbox object used for collision checks
 
     public Entity() {
-        this.name = "";
-        this.xPos = 0;
-        this.yPos = 0;
-        this.moving = false;
-        this.hitbox = null;
+        // default constructor for any basic entity
+        this.name = "";// starts the name as blank
+        this.xPos = 0;// starts x position at 0
+        this.yPos = 0;// starts y position at 0
+        this.moving = false;// starts as not moving
+        this.hitbox = null;// starts with no hitbox until one is added
     }
 
     public Entity(String name, int xPos, int yPos, boolean moving, Hitbox hitbox) {
-        this.name = name;
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.moving = moving;
-        this.hitbox = hitbox;
+        // constructor that lets all entity data be recived in at once
+        this.name = name;// stores the name
+        this.xPos = xPos;// stores the x position
+        this.yPos = yPos;// stores the y position
+        this.moving = moving;// stores if it is moving or not
+        this.hitbox = hitbox;// stores the hitbox object
     }
-
+    //getters
     public String getName() {
         return name;
     }
-
+    //setters
     public void setName(String name) {
         this.name = name;
     }
-
+    //getters
     public int getXPos() {
         return xPos;
     }
-
+    //setters
     public void setXPos(int xPos) {
         this.xPos = xPos;
     }
-
+    //getters
     public int getYPos() {
         return yPos;
     }
-
+    //setters
     public void setYPos(int yPos) {
         this.yPos = yPos;
     }
-
+    //getters
     public boolean isMoving() {
         return moving;
     }
-
+    //setters
     public void setMoving(boolean moving) {
         this.moving = moving;
     }
-
+    //getters
     public Hitbox getHitbox() {
         return hitbox;
     }
-
+    //setters
     public void setHitbox(Hitbox hitbox) {
         this.hitbox = hitbox;
     }
